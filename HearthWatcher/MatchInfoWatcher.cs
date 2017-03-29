55b0f8113929df4f58ed90d173d484ab55d7209a
Alexander Zeier <alex@hearthsim.net>
2017-03-29 22:19:52 +0200

    Add MatchInfoWatcher

---
+++ HearthWatcher/MatchInfoWatcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HearthMirror;
+using HearthMirror.Objects;
+
+namespace HearthWatcher
+{
+	public class MatchInfoWatcher : Watcher
+	{
+		public event Action<MatchInfo> OnMatchInfoChanged;
+		public event Action<GameServerInfo> OnGameServerInfoChanged;
+
+		private MatchInfo _matchInfo;
+		private GameServerInfo _gameServerInfo;
+
+		protected override void Reset()
+		{
+			_matchInfo = null;
+			_gameServerInfo = null;
+		}
+
+		public override void Update()
+		{
+			//TODO: Fix comparisons, '!=' always returns true since they are different objects
+			var matchInfo = Reflection.GetMatchInfo();
+			if(matchInfo != _matchInfo)
+			{
+				_matchInfo = matchInfo;
+				OnMatchInfoChanged?.Invoke(_matchInfo);
+			}
+
+			var gameServerInfo = Reflection.GetServerInfo();
+			if(gameServerInfo != _gameServerInfo)
+			{
+				_gameServerInfo = gameServerInfo;
+				OnGameServerInfoChanged?.Invoke(gameServerInfo);
+			}
+		}
+	}
+}
